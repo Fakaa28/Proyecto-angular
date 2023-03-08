03e9ca594757321d0dd770aaccd0862238a5a52a
Facundo <fakamadafaka_2004@hotmail.com>
2023-03-07 21:34:05 -0300

    Agregado codificacion y estilo de las tareas

---
+++ angular/angular_proyecto/src/app/components/header/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {Task} from '../../../../Task'
+import {TASKS} from '../../../../mock-tasks'
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent {
+@Input() task: Task = TASKS[0]
+}
