01f05c38474b997cdcfbf8fbf2dc2cb8787a317a
Facundo <fakamadafaka_2004@hotmail.com>
2023-03-14 00:08:42 -0300

    Fin del desarrollo inicial de mi lista de tareas en Angular

--- angular/angular_proyecto/src/app/components/header/header.component.ts
+++ angular/angular_proyecto/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -6,14 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title:string= "Mi proyecto en Angular"
+  title:string= "Mi lista de TAREAS"
+  showAddTask:boolean = true;
+  subscription?: Subscription;
 
-constructor() { }
+constructor(
+  private uiService:UiService,
+  private router:Router,
+) { 
+this.subscription = this.uiService.OnToggle()
+ 
+       .subscribe( value => this.showAddTask = value ) 
+
+}
 
 ngOnInit(): void {
 
 }
 toggleAddTask(){
-  console.log("Tarea Añadida!");
+  this.uiService.toggleAddTask();
 }
+
+hasRoute(route:string) {
+
+return this.router.url === route
+
+}
+
 }
